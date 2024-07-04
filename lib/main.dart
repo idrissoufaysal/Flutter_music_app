7ed93d033e977544daa46f99294ba285273ee3a9
idrissou <idrissoufaysal93@.com>
2024-07-04 20:36:56 +0000

    add Maitre gims

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Coda Music'),
+      home: const MyHomePage(title: 'Maitre gims'),
     );
   }
 }
